1ab88009f920bf0ae316fd44ebc7cf2b21595b0d
Yogesh Bhatt <65986357+ygshbht@users.noreply.github.com>
2024-07-06 09:11:30 +0530

    Add portfinder functionality

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,56 +3,63 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const express = require("express");
 const path = require("path");
 const webpack = require("webpack"); // only add this if you don't have yet
+const portfinder = require("portfinder");
 
 require("dotenv").config({ path: "./.env.development" });
 require("dotenv").config({ path: "./.env" });
 
 //maybe add subsequent .env.local, etc. files based on prcedense
 
-module.exports = {
-  // mode defaults to 'production' if not set
-  mode: mode,
-  entry: "./src/index.ts",
-  // entry not required if using `src/index.js` default
-  // output not required if using `dist/main.js` default
+module.exports = async () => {
+  const defaultPort = 3000;
+  const port = await portfinder.getPortPromise({ port: defaultPort });
 
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
+  return {
+    // mode defaults to 'production' if not set
+    mode: mode,
+    entry: "./src/index.ts",
+    // entry not required if using `src/index.js` default
+    // output not required if using `dist/main.js` default
 
-      {
-        test: /\.glsl$/i,
-        loader: "html-loader",
-      },
-    ],
-  },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
 
-  devtool: "source-map",
-  resolve: {
-    extensions: [".ts", ".js", ".json"],
-  },
-
-  // required if using webpack-dev-server
-  devServer: {
-    // contentBase: "./dist",
-    port: 3000,
-    setupMiddlewares: (middlewares, devServer) => {
-      devServer.app.use(
-        "/",
-        express.static(path.resolve(__dirname, "./public"))
-      );
-      return middlewares;
+        {
+          test: /\.glsl$/i,
+          loader: "html-loader",
+        },
+      ],
     },
-    open: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: "./template.htm" }),
-    new webpack.DefinePlugin({
-      "process.env": JSON.stringify(process.env),
-    }),
-  ],
+
+    devtool: "source-map",
+    resolve: {
+      extensions: [".ts", ".js", ".json"],
+    },
+
+    // required if using webpack-dev-server
+    devServer: {
+      // contentBase: "./dist",
+
+      port,
+      setupMiddlewares: (middlewares, devServer) => {
+        devServer.app.use(
+          "/",
+          express.static(path.resolve(__dirname, "./public"))
+        );
+        return middlewares;
+      },
+      open: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: "./template.htm" }),
+      new webpack.DefinePlugin({
+        "process.env": JSON.stringify(process.env),
+      }),
+    ],
+  };
 };
